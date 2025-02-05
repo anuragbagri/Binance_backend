aeecae56504f3fea101d5b7d61e04b807b9559f9
anurag bagri <anurag123bagri@gmail.com>
2025-02-05 10:14:06 +0530

    file setup --backend

---
+++ Backend/src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+const app= express();
+
+
+app.post("/signup" , (req, res) => {
+
+})
+
+app.get("/depositAddress/:Userid" , (req,res) => {
+
+})
+
+app.listen(3000);
